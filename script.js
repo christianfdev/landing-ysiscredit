039942bd8ed5c70b5473c0e9070d7b4a038e0703
christianfdev <chriis_farias@hotmail.com>
2022-12-27 17:45:58 -0300

    Initial responsivity

--- script.js
+++ script.js
@@ -2,14 +2,17 @@ const btnMobile = document.getElementById('btn-mobile');
 
 function toggleMenu(event) {
   if (event.type === 'touchstart') event.preventDefault();
+  const introduction = document.getElementById('introduction');
   const nav = document.getElementById('nav');
   nav.classList.toggle('active');
   const active = nav.classList.contains('active');
   event.currentTarget.setAttribute('aria-expanded', active);
   if (active) {
     event.currentTarget.setAttribute('aria-label', 'Fechar Menu');
+    introduction.classList.toggle('alter-padding');
   } else {
     event.currentTarget.setAttribute('aria-label', 'Abrir Menu');
+    introduction.classList.toggle('alter-padding');
   }
 }
 
